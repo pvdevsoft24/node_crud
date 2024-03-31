997b7609c1c9ea4214787ac61503fbdacc923870
Vyas <pvdevsoft@gmail.com>
2024-03-31 23:04:21 +0530

    added model

--- index.js
+++ index.js
@@ -1,11 +1,25 @@
 const express = require('express')
+const mongoose = require('mongoose')
+
 
 // init express app 
 
 const app  = express();
 
+
+
 // init server 
 
 app.listen(3000,() =>{
     console.log('server listening to port 3000');
+});
+
+// connecting to DB
+
+mongoose.connect('mongodb://localhost:27017/AddressBook',
+{useNewUrlParser: true, useUnifiedTopology:true
+}).then(() =>{
+    console.log('connected to db')
+}).catch((error) =>{
+    console.log(error)
 });
