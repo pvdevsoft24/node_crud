7710dfc92a4e30918810917d9729af1d3e82bf97
Vyas <pvdevsoft@gmail.com>
2024-03-31 23:49:58 +0530

    added http method

--- README.md
+++ README.md
@@ -1,1 +1,10 @@
 reference :https://amalj07.medium.com/crud-app-in-node-js-and-mongodb-1587923153d6
+
+
+To start the server :
+
+node index.js
+
+To stop the server :
+
+ctrl + c
--- index.js
+++ index.js
@@ -1,18 +1,19 @@
 const express = require('express')
 const mongoose = require('mongoose')
+const Address = require('./model/model')
+const bodyParser = require('body-parser')
+
 
 
 // init express app 
 
 const app  = express();
 
+// parse application/x-www-form-urlencoded
+
+app.use(bodyParser.urlencoded({rxtended:false}))
 
 
-// init server 
-
-app.listen(3000,() =>{
-    console.log('server listening to port 3000');
-});
 
 // connecting to DB
 
@@ -22,4 +23,41 @@ mongoose.connect('mongodb://localhost:27017/AddressBook',
     console.log('connected to db')
 }).catch((error) =>{
     console.log(error)
+});
+
+//  Reading a user from AddressBook
+
+app.get('/:id',(req,res) =>{
+    Address.findById(req.params.id,(err,user) =>{
+        res.send(user)
+    })
+})
+
+// Adding a user to AddressBook
+
+app.post('/', (req,res) => {
+    name = req.body.name,
+    email = req.body.email,
+    phone = req.body.phone,
+    place = req.body.place
+
+    let newAddress = new Address({
+        name:name,
+        email:email,
+        phone:phone,
+        place:place
+    })
+
+    newAddress.save().then((address) =>{
+        res.send(address)
+    }).catch((err) =>{
+        console.log(error)
+    })
+})
+
+
+// init server 
+
+app.listen(3000,() =>{
+    console.log('server listening to port 3000');
 });
