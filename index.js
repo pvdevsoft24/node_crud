4c1af9df0d5654a6e85f3e1671615005ece9bf73
Vyas <pvdevsoft@gmail.com>
2024-03-31 20:36:12 +0530

    init commit

---
+++ index.js
@@ -0,0 +1,11 @@
+const express = require('express')
+
+// init express app 
+
+const app  = express();
+
+// init server 
+
+app.listen(3000,() =>{
+    console.log('server listening to port 3000');
+});
